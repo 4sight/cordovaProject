9faa83bb247a11b575920d690a46626803bf1261
Greeley Dawson <gdawson@gdawson.audioeye.com>
2020-02-24 11:18:43 -0500

    removed import.js

--- platforms/android/app/src/main/assets/www/js/main.js
+++ platforms/android/app/src/main/assets/www/js/main.js
@@ -1,3 +1,16 @@
+// const map = new ol.Map({
+//   target: 'map',
+//   layers: [
+//     new ol.layer.Tile({
+//       source: new ol.source.OSM()
+//     })
+//   ],
+//   view: new ol.View({
+//     center: [0, 0],
+//     zoom: 0
+//   })
+// });
+
 document.getElementById("getPosition").addEventListener("click", getPosition);
 document.getElementById("watchPosition").addEventListener("click", watchPosition);
 document.getElementById("currentWeather").addEventListener("click", currentWeather);
@@ -74,17 +87,82 @@ function currentWeather() {
   })
 })}};
 
-function readFile(fileEntry) {
+// const selectedFile = document.getElementById('input').files[0];
+document.getElementById('input').addEventListener('change', function(){
+  var reader = new FileReader();
+  reader.onload = function(){
+    document.getElementById('displayBox').textContent = this.result;
+  };
+  reader.readAsText(this.files[0]);
+});
 
-    fileEntry.file(function (file) {
-        var reader = new FileReader();
+// Geolocation
 
-        reader.onloadend = function() {
-            console.log("Successful file read: " + this.result);
-            displayFileData(fileEntry.fullPath + ": " + this.result);
-        };
+var view = new ol.View({
+  center: [0, 0],
+  zoom: 2
+});
 
-        reader.readAsText(file);
+var geolocation = new ol.Geolocation({
+  // enableHighAccuracy must be set to true to have the heading value.
+  trackingOptions: {
+    enableHighAccuracy: true
+  },
+  projection: view.getProjection()
+});
 
-    }, onErrorReadFile);
+function el(id) {
+  return document.getElementById(id);
 }
+
+el('track').addEventListener('change', function() {
+  geolocation.setTracking(this.checked);
+});
+
+// update the HTML page when the position changes.
+geolocation.on('change', function() {
+  el('accuracy').innerText = geolocation.getAccuracy() + ' [m]';
+  el('altitude').innerText = geolocation.getAltitude() + ' [m]';
+  el('altitudeAccuracy').innerText = geolocation.getAltitudeAccuracy() + ' [m]';
+  el('heading').innerText = geolocation.getHeading() + ' [rad]';
+  el('speed').innerText = geolocation.getSpeed() + ' [m/s]';
+});
+
+// handle geolocation error.
+geolocation.on('error', function(error) {
+  var info = document.getElementById('info');
+  info.innerHTML = error.message;
+  info.style.display = '';
+});
+
+var accuracyFeature = new ol.Feature();
+geolocation.on('change:accuracyGeometry', function() {
+  accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+});
+
+var positionFeature = new ol.Feature();
+positionFeature.setStyle(new ol.style.Style({
+  image: new ol.style.Circle({
+    radius: 6,
+    fill: new ol.style.Fill({
+      color: '#3399CC'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#fff',
+      width: 2
+    })
+  })
+}));
+
+geolocation.on('change:position', function() {
+  var coordinates = geolocation.getPosition();
+  positionFeature.setGeometry(coordinates ?
+    new Point(coordinates) : null);
+});
+
+new ol.source.Vector({
+  map: map,
+  source: new ol.source.Vector({
+    features: [accuracyFeature, positionFeature]
+  })
+});
--- www/js/imports.js
+++
@@ -1,4 +0,0 @@
-// import Map from 'https://dev.jspm.io/npm:ol@5.0.0-beta.12/Map.js';
-// import View from 'https://dev.jspm.io/npm:ol@5.0.0-beta.12/View.js';
-// import TileLayer from 'https://dev.jspm.io/npm:ol@5.0.0-beta.12/layer/Tile.js';
-// import OSM from 'https://dev.jspm.io/npm:ol@5.0.0-beta.12/source/OSM.js';
